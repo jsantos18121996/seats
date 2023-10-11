8a5893157550dabb3d166e7d6e39677c187ca597
jaime <jhorlyn.santos@costamar.com>
2023-10-10 19:07:37 -0500

    Se agregan url al entorno de prod

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,10 @@
 module.exports = {
     variables: {
         servicesEndpoint: {
-            search: 'http://ec2-18-118-150-199.us-east-2.compute.amazonaws.com:8080/pests-service/reportOne'
+            search: 'http://ec2-18-118-150-199.us-east-2.compute.amazonaws.com:8080/pests-service/reportOne',
+            reportOne: 'http://ec2-3-136-154-5.us-east-2.compute.amazonaws.com:8080/pests-service/reportOne',
+            reportTwo: 'http://ec2-3-136-154-5.us-east-2.compute.amazonaws.com:8080/pests-service/reportTwo',
+            reportThree: 'http://ec2-3-136-154-5.us-east-2.compute.amazonaws.com:8080/pests-service/reportThree'
         }
     },
     allowedOrigins: ['https://seats-frontend.netlify.app', 'http://localhost:3000']
